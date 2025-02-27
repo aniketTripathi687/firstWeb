8587ace3c1a1410ceb7fb1ae4efd86e82ca7330b
aniketTripathi687 <a.tripathi@students.htl-leonding.ac.at>
2025-02-27 10:10:09 +0100

    did stuff

---
+++ scripts/morse.js
@@ -0,0 +1,17 @@
+﻿const name = 'Rudi';
+
+function init(){
+    const ayan = {
+        friends: [
+            'Rudi', 'Dave', 'Laurenz'
+        ]
+    };
+
+    for(const prop of ayan){
+        console.log(prop);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', event =>{
+    init();
+});
